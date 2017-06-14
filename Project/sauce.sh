dd54912f3368eac6bc961988c70baf09ec15f54c
davidcmonty <davidcmonty@gmail.com>
2017-06-14 18:02:42 +1200

    Change project name

--- Project/sauce.sh
+++ Project/sauce.sh
@@ -8,15 +8,15 @@ done
 
 echo
 echo ==============================================================================
-echo Sauce Connect is now up and the tests are being executed on Saucelabs
+echo Sauce Connect is now running and the tests are being executed on Saucelabs
 echo ==============================================================================
 echo
 
 echo Win10 + E11:
-robot -v REMOTE_URL:http://jeremybates:bca8f2e1-acae-4742-952d-ce1afdfc95b2@ondemand.saucelabs.com:80/wd/hub -d results/Win10+IE11 -i test -v DESIRED_CAPABILITIES:"name:Win10 + IE11,platform:Windows 8.1,browserName:internet explorer,version:11.0" Tests/project.robot
+robot -v REMOTE_URL:http://jeremybates:bca8f2e1-acae-4742-952d-ce1afdfc95b2@ondemand.saucelabs.com:80/wd/hub -d results/Win10+IE11 -i test -v DESIRED_CAPABILITIES:"name:Win10 + IE11,platform:Windows 8.1,browserName:internet explorer,version:11.0" Tests/demo.robot
 
 echo Win10 + Chrome58:
-robot -v REMOTE_URL:http://jeremybates:bca8f2e1-acae-4742-952d-ce1afdfc95b2@ondemand.saucelabs.com:80/wd/hub -d results/Win10+Chrome58 -i test -v DESIRED_CAPABILITIES:"name:Win10 + Chrome,platform:Windows 10,browserName:chrome,version:58.0" Tests/project.robot
+robot -v REMOTE_URL:http://jeremybates:bca8f2e1-acae-4742-952d-ce1afdfc95b2@ondemand.saucelabs.com:80/wd/hub -d results/Win10+Chrome58 -i test -v DESIRED_CAPABILITIES:"name:Win10 + Chrome,platform:Windows 10,browserName:chrome,version:58.0" Tests/demo.robot
 
 #echo iPhone 6s
 #robot -v REMOTE_URL:http://jeremybates:bca8f2e1-acae-4742-952d-ce1afdfc95b2@ondemand.saucelabs.com:80/wd/hub -d results/Win10+Chrome40 -i test -v DESIRED_CAPABILITIES:"name:iPhone 6s,appiumVersion:1.6.4,deviceName:iPhone 6s Simulator,deviceOrientation:portrait,platformVersion:10.3,platformName:iOS,browserName:Safari" tests/project.robot
